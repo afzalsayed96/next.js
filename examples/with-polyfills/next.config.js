01c3c627634f3cc94bbaab09beb51ceeb4cd0d85
Afzal Sayed <14029371+afzalsayed96@users.noreply.github.com>
2020-06-25 16:48:13 +0530

    Update next.config.js

--- examples/with-polyfills/next.config.js
+++ examples/with-polyfills/next.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  webpack: function (cfg) {
-    const originalEntry = cfg.entry
-    cfg.entry = async () => {
+  webpack: function (cfg) {  
+    const originalEntry = config.entry
+    config.entry = async () => {
       const entries = await originalEntry()
-
-      if (
-        entries['main.js'] &&
-        !entries['main.js'].includes('./client/polyfills.js')
-      ) {
-        entries['main.js'].unshift('./client/polyfills.js')
+      if (entries['static/runtime/polyfills.js'] && !entries['static/runtime/polyfills.js'].includes('./client/polyfills.js')) {
+        entries['static/runtime/polyfills.js'] = [ entries['static/runtime/polyfills.js'], './client/polyfills.js' ].flatMap((e) => e)
       }
 
       return entries
